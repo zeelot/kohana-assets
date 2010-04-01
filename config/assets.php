520e928618fe7a8ab5a5021a093ebfffd825f5b6
Lorenzo Pisani <zeelot3k@gmail.com>
2010-04-01 13:58:23 -0700

    adding config file

---
+++ config/assets.php
@@ -0,0 +1,25 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * This is where assets and asset dependencies are defined.
+ * 
+ * rules:
+ * directories - if a view is used in one of these directories, the asset is loaded
+ * views - if any of the views are used, the asset is loaded
+ * regex - if the full view path matches this regex, the asset is loaded
+ * exclude_directories - a list of directories to exclude from rules
+ * exclude_views - a list of views to exclude from rules
+ * exclude_regex - if the full view path matches this regex, the asset is not loaded
+ *
+ * @package    YurikoCMS
+ * @author     Lorenzo Pisani - Zeelot
+ * @copyright  (c) 2008-2010 Lorenzo Pisani
+ * @license    http://yurikocms.com/license
+ *
+ * Thanks Ebonhand for the feedback and great ideas.
+ */
+
+return array
+(
+	
+);
