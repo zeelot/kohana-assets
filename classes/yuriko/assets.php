9729e52916be5d48ffaf1b0dc93184576910eab4
Lorenzo Pisani <zeelot3k@gmail.com>
2010-04-17 06:35:11 +0800

    replaced all the inclusion/exclusion functionality with a regex pattern

--- classes/yuriko/assets.php
+++ classes/yuriko/assets.php
@@ -44,7 +44,7 @@ class Yuriko_Assets {
 	public function render()
 	{
 		// calculate the dependencies just once
-		( ! $this->_loaded_dependencies) AND $this->load_dependencies();
+		! $this->_loaded_dependencies AND $this->load_dependencies();
 
 		// sort the assets
 		usort($this->_assets, array($this, 'sort_assets'));
@@ -119,120 +119,24 @@ class Yuriko_Assets {
 
 		// the Views that where used
 		$views = (array)View::$loaded;
+		$view_string = ';'.implode(';', $views);
 
 		// the assets that where defined
 		$assets = (array)Kohana::config('assets');
 
 		foreach ($assets as $key => $asset)
 		{
-			// wether or not to include this set of assets
-			$pass = FALSE;
+			$pattern = Arr::get($asset, 'pattern');
+			if ($pattern === FALSE)
+				continue;
 
-			$testing_views = $views;
-			
-			$exclude_views = Arr::get($asset['rules'], 'exclude_views');
-			$exclude_dirs = Arr::get($asset['rules'], 'exclude_directories');
-			$exclude_regex = Arr::get($asset['rules'], 'exclude_regex');
+			// switch any leading ^ to a ; (to match path beginnings in our view_string)
+			if (substr($pattern, 0, 1) === '^')
+				$pattern = ';'.substr($pattern, 1);
 
-			$include_views = Arr::get($asset['rules'], 'views');
-			$include_dirs = Arr::get($asset['rules'], 'directories');
-			$include_regex = Arr::get($asset['rules'], 'regex');
-
-			// @TODO: cache stuff
-
-			// check exclude rules first
-			if ($exclude_views)
-			{
-				// unset any views that are excluded so we don't them
-				$testing_views = array_diff($testing_views, $exclude_views);
-			}
-
-			if ($exclude_regex)
-			{
-				// @TODO: implement
-			}
-
-			if ($exclude_dirs)
-			{
-				// unset any views that are in these directories
-				$to_unset = array();
-
-				foreach ($testing_views as $v)
-				{
-					if ($this->is_in_directories($exclude_dirs, $v))
-					{
-						$to_unset[] = $v;
-					}
-				}
-
-				$testing_views = array_diff($testing_views, $to_unset);
-			}
-
-			// start testing inclusion rules (only need one match)
-			if ($include_views)
-			{
-				$diff = array_diff($testing_views, $include_views);
-				
-				// if $diff is smaller, we had at least one match
-				if (count($diff) < count($testing_views))
-				{
-					$this->add_group($key);
-
-					// skip to next asset
-					continue;
-				}
-			}
-
-			if ($include_dirs)
-			{
-				if ($this->is_in_directories($include_dirs, $testing_views))
-				{
-					$this->add_group($key);
-					
-					// skip to next asset
-					continue;
-				}
-			}
-
-			// check regex last for performance reasons
-			if ($include_regex)
-			{
-				// @TODO: implement
-			}
+			if (preg_match($pattern, $view_string))
+				$this->add_group($key);
 		}
 		$this->_loaded_dependencies = TRUE;
-	}
-
-	/**
-	 * Checks if a view is in any of the given directories
-	 *
-	 * @param array $directories
-	 * @param string $view - the view path
-	 * @return bool
-	 */
-	public function is_in_directories(array $directories, array $views)
-	{
-		foreach ($directories as $dir)
-		{
-			foreach ($views as $view)
-			{
-				// check if $dir is in the path of $view
-				if (strpos(dirname($view), $dir) !== FALSE)
-					return TRUE;
-			}
-		}
-		return FALSE;
-	}
-
-	/**
-	 *
-	 * @param String $regex - the regex to test all the views against
-	 * @param array $views - the array of view paths to test
-	 * @return Bool
-	 */
-	public function matches_regex(array $regexes, array $views)
-	{
-		// @TODO: implement
-		return FALSE;
 	}
 } // End Assets
